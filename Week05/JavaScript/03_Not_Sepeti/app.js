db3940b8e1ea0e1ab5b103066c04b766c2d7efe7
Harun Özer <harun_ozer1@hotmail.com>
2022-11-02 23:30:15 +0300

    ödev

--- Week05/JavaScript/03_Not_Sepeti/app.js
+++ Week05/JavaScript/03_Not_Sepeti/app.js
@@ -1,46 +1,77 @@
 let btnAdd = document.getElementById('btnAddNewNote');
 let txtNoteName = document.getElementById('txtNoteName');
-btnAdd.addEventListener('click', newNote);
+let completeId;
 
-txtNoteName.addEventListener('keypress', function(event){
+btnAdd.addEventListener('click', newTask);
+txtNoteName.addEventListener('keypress', function(event) {
     if (event.key == 'Enter') {
         event.preventDefault();
         btnAdd.click();
     }
 });
-
 let notListesi = [
-    {'id': 1, 'notAdi': 'Not 1'},
-    {'id': 2, 'notAdi': 'Not 2'},
-    {'id': 3, 'notAdi': 'Not 3'}
+    { 'id': 1, 'notAdi': 'Not 1' },
+    { 'id': 2, 'notAdi': 'Not 2' },
+    { 'id': 3, 'notAdi': 'Not 3' },
+    { 'id': 4, 'notAdi': 'Not 4' }
 ];
 
-let ul = document.getElementById('not-list')
-
 function displayNotes() {
-    let ul = document.getElementById('not-list');
+    let ul = document.getElementById('note-list')
     ul.innerHTML='';
+    if (notListesi.length == 0) {
+        ul.innerHTML='<span style = "color:white; font-size:25px;">Not Listeniz boş!</span>';
+    }
+    for (const not of notListesi) {
+        let li = `
+        <li class="note">
+        <label for=""><input class="txtNotGir" type="text" value="${not.notAdi}" id="${not.id}" readonly></label>
+        <button id="completeButton" onclick="completeNote()"><i class="fa-solid fa-square-check" style="color: white; font-size:20px;"></i></button>
+        <button id="deleteButton" onclick="removeNote(${not.id})"><i class="fa-solid fa-trash-can" style="color: white; font-size: 17px;"></i></button>
+        </li>
+        `;
+        ul.insertAdjacentHTML('beforeend', li);
+    }
 }
-
-for (const not of notListesi) {
-    let li = `
-    <li class="note">
-    <label for=""><input type="text" value="${not.notAdi}" id="${not.id}"></label>
-    <button id="completeButton"><i class="fa-solid fa-square-check" style="color: white; font-size:20px;"></i></button>
-    <button id="deleteButton"><i class="fa-solid fa-trash-can" style="color: white; font-size: 17px;"></i></button>
-    </li>`;
-    ul.insertAdjacentHTML('beforeend', li);
-}
-
-function newNote(event) {
+function newTask (event) {
     event.preventDefault();
+    if (isFull(txtNoteName.value)) {
         notListesi.push(
             {
-                'id': notListesi.length + 1,
-                'notAdi': txtNoteName.value
+                'id': notListesi.length+1,
+                'notAdi': ilkHarfiBuyut(txtNoteName.value)
             }
         )
-    txtNoteName.value = '';
+        displayNotes();
+    }else {
+        alert('Lütfen boş bırakmayınız!')
+    }
+    txtNoteName.value='';
     txtNoteName.focus();
+};
+
+function isFull (value) {
+    if (value.trim() =='') {
+        return false;
+    }
+    return true;
+};
+
+function ilkHarfiBuyut(value) {
+    let ilkHarf = value[0].toUpperCase();
+    let geriKalan = value.slice(1);
+    return ilkHarf+geriKalan;
+};
+
+function removeNote(id) {
+    let deletedId;
+    for (const notIndex in notListesi) {
+        if (notListesi[notIndex].id == id) {
+            deletedId = notIndex;
+        }
+    };
+    notListesi.splice(deletedId, 1);
+    displayNotes();
 }
+
 displayNotes();
