96b484ff12a1ebeff9a06634240351a54a7cb087
Harun Özer <harun_ozer1@hotmail.com>
2022-12-14 18:05:20 +0300

    14.12

---
+++ Week11/13-12-2022/WebApplication1/Services/SendGridEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Services
+{
+    public class SendGridEmailService : IEmailService
+    {
+        public void Send(string to, string message, string topic)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
